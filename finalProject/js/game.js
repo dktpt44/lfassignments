2df0615efc074c8405f7367338c9fe73dda47bac
Dhurba Tripathi <dhurba6don@gmail.com>
2019-12-13 16:03:28 +0545

    character animated

--- finalProject/js/game.js
+++ finalProject/js/game.js
@@ -2,21 +2,30 @@ class Game {
   constructor() {
     this.mapContainer = document.getElementsByClassName('map-container')[0];
     this.pathContainer = document.getElementsByClassName('path-container')[0];
+    this.player;
+    this.playerIndex = 1;
+    this.changeLate = 0;
     this.oneRunLength;
-    this.gameSpeed = 0.6;
+    this.gameSpeed = 2;
     this.map = [];
-    this.bottomPosition = [0, 59, 103, 134, 156, 173, 184, 192];
-    this.bottomDecreaser=[];
+    this.bottomPosition = [0, 59, 103, 134, 156, 173, 185, 194];
+    this.bottomDecreaser = [];
     this.leftPosition = [0, 67, 115, 150, 176, 193, 203, 210];
-    this.widths = [500, 362, 262, 190, 137, 103, 82, 67];
-    this.widthIncreaser=[];
-    this.heights = [60, 45, 32, 23, 18, 12, 9, 6];
-    this.heightIncreaser=[];
+    this.widths = [500, 362, 262, 190, 137, 103, 83, 69];
+    this.widthIncreaser = [];
+    this.heights = [60, 45, 32, 23, 18, 13, 10, 7];
+    this.heightIncreaser = [];
     this.timeframe;
     this.gameInterval;
     this.start();
   }
+
   start = function () {
+    this.player = new Player(this.pathContainer);
+    this.player.changeImg(this.playerIndex);
+    this.player.isInAir = false;
+    this.player.isMovingLR=false;
+    document.addEventListener('keydown', this.keyInp.bind(this));
     this.timeframe = 600 / this.gameSpeed;   //time taken in millis
     var ix;
     while (this.map.length < 8) {
@@ -25,9 +34,9 @@ class Game {
         this.leftPosition[indx], this.widths[indx], this.heights[indx]);
       this.map.push(newMapE);
     }
-    for(ix=0;ix<7;ix++){
-      this.widthIncreaser.push((this.widths[ix]- this.widths[ix + 1]) * 10 / this.timeframe);
-      this.heightIncreaser.push((this.heights[ix] - this.heights[ix + 1]) * 10/ this.timeframe);
+    for (ix = 0; ix < 7; ix++) {
+      this.widthIncreaser.push((this.widths[ix] - this.widths[ix + 1]) * 10 / this.timeframe);
+      this.heightIncreaser.push((this.heights[ix] - this.heights[ix + 1]) * 10 / this.timeframe);
       this.bottomDecreaser.push((this.bottomPosition[ix + 1] - this.bottomPosition[ix]) * 10 / this.timeframe);
     }
     this.gameInterval = setInterval(this.startGame.bind(this), 10);
@@ -38,6 +47,16 @@ class Game {
       var newMapE = new Map(this.pathContainer, this.bottomPosition[indx],
         this.leftPosition[indx], this.widths[indx], this.heights[indx]);
       this.map.push(newMapE);
+    }
+    this.changeLate++;
+    if (this.changeLate % 5 == 0) {
+      this.changeLate = 0;
+      if (this.playerIndex == 11) {
+        this.playerIndex = 0;
+      }
+      this.playerIndex++;
+      this.player.changeImg(this.playerIndex);
+
     }
     var mapIndex;
     for (mapIndex = 0; mapIndex < this.map.length; mapIndex++) {
@@ -51,20 +70,40 @@ class Game {
       var newLeft = currentMap.leftPos;
       var newBot = currentMap.botPos;
       if (mapIndex == 0) {
-        // console.log(timeTaken);
-        newWidth += 1760 / this.timeframe;
-        newLeft -= 880 / this.timeframe;
+        newWidth += 1840 / this.timeframe;
+        newLeft -= 910 / this.timeframe;
         newBot -= this.gameSpeed;
       } else {
-        var widIncr = this.widthIncreaser[mapIndex-1];
+        var widIncr = this.widthIncreaser[mapIndex - 1];
         newWidth += widIncr;
         newLeft -= widIncr / 2;
-        newHeight += this.heightIncreaser[mapIndex-1];
-        newBot -= this.bottomDecreaser[mapIndex-1];
+        newHeight += this.heightIncreaser[mapIndex - 1];
+        newBot -= this.bottomDecreaser[mapIndex - 1];
       }
       currentMap.setMapAttrs(newWidth, newHeight, newLeft, newBot);
       currentMap.moveMap();
     }
+  }
+  keyInp = function () {
+    if(this.player.isInAir || this.player.isMovingLR) {
+      return;
+    }
+    var pressedKey = event.keyCode;
+    if (pressedKey == 38) {
+      this.player.isInAir=true;
+      this.player.jumpInAir();
+    } else if (pressedKey == 37) {
+      this.player.isMovingLR=true;
+      this.player.moveLeft();
+
+    } else if (pressedKey == 39) {
+      this.player.isMovingLR=true;
+      this.player.moveRight();
+
+    } else {
+      return;
+    }
+
   }
 
   renderMap = function () {
--- finalProject/js/player.js
+++ finalProject/js/player.js
@@ -1,26 +1,94 @@
 class Player {
   constructor(parElem) {
-    this.playerElem;
-    this.width;
-    this.height;
-    this.left=leftPos;
-    this.bottom=bottomPos;
-    this.parElem=parElem;
+    this.playerElement;
+    this.width = 60;
+    this.height = 150;
+    this.left = 220;
+    this.bottom = 20;
+    this.parElem = parElem;
     this.playerImage;
     this.makeplayerImg();
+    this.isMovingLR;
+    this.isInAir;
+    this.jumpHeight = 80;
   }
-  makeplayerImg = function() {
+  makeplayerImg = function () {
     this.playerElement = document.createElement('div');
-    this.playerElement.style.width=this.width+'px';
-    this.playerElement.style.height=this.height+'px';
-    this.playerElement.style.position='absolute';
-    this.playerElement.style.bottom=this.bottom+'px';
-    this.playerElement.style.left=this.left+'px';
+    this.playerElement.style.width = this.width + 'px';
+    this.playerElement.style.height = this.height + 'px';
+    this.playerElement.style.position = 'absolute';
+    this.playerElement.style.zIndex = '2';
+    this.playerElement.style.bottom = this.bottom + 'px';
+    this.playerElement.style.left = this.left + 'px';
     this.parElem.appendChild(this.playerElement);
     this.playerImage = document.createElement('img');
-    this.playerImage.style.width=this.width+'px';
-    this.playerImage.style.height=this.height+'px';
-    this.playerImage.setAttribute('src','xx');
+    this.playerImage.style.width = this.width + 'px';
+    this.playerImage.style.height = this.height + 'px';
     this.playerElement.appendChild(this.playerImage);
   }
+  changeImg = function (indx) {
+    var srcImg = 'images/run/' + indx + '.png';
+    this.playerImage.setAttribute('src', srcImg);
+  }
+  jumpInAir = function () {
+    var newPos = this.bottom + this.jumpHeight;
+    var jumpInterval = setInterval(startAnim.bind(this), 10);
+    function startAnim() {
+      if (this.bottom > newPos) {
+        clearInterval(jumpInterval);
+        this.jumpDown();
+      } else {
+        this.bottom += 5;
+        this.playerElement.style.bottom = this.bottom + 'px';
+      }
+    }
+  }
+  jumpDown = function () {
+    var newPos = 20;
+    var jumpInterval = setInterval(startAnim.bind(this), 10);
+    function startAnim() {
+      if (this.bottom < newPos) {
+        clearInterval(jumpInterval);
+        this.isInAir=false;
+      } else {
+        this.bottom -= 5;
+        this.playerElement.style.bottom = this.bottom + 'px';
+      }
+    }
+  }
+  moveLeft = function () {
+    var newPos = this.left-50;
+    var leftInterval = setInterval(startAnim.bind(this), 10);
+    function startAnim() {
+      if (this.left < newPos) {
+        clearInterval(leftInterval);
+        if(this.isMovingLR){
+          this.moveRight();
+        }
+        this.isMovingLR=false;
+      } else {
+        this.left -= 5;
+        this.playerElement.style.left = this.left + 'px';
+      }
+    }
+    
+  }
+  moveRight=function () {
+    var newPos = this.left+50;
+    var leftInterval = setInterval(startAnim.bind(this), 10);
+    function startAnim() {
+      if (this.left > newPos) {
+        clearInterval(leftInterval);
+        if(this.isMovingLR){
+          this.moveLeft();
+        }
+        this.isMovingLR=false;
+      } else {
+        this.left += 5;
+        this.playerElement.style.left = this.left + 'px';
+      }
+    }
+    
+  }
+
 }
